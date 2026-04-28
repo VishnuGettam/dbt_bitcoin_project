0662060ae87058c0d6deab3369ada2e4810f3431
vishnugettam <vishnugettam@gmail.com>
2026-04-28 21:19:09 +0530

    models creation

---
+++ models/stage/bitcoin_stg.sql
@@ -0,0 +1,25 @@
+{{
+    config(alias='tblbitcoin_stg')
+}}
+
+
+with bitcoin_stg as (
+
+
+select  
+"hash"::varchar as hashkey,
+"block_hash"::varchar as block_hash,
+"block_number"::int as block_number,
+"block_timestamp"::timestamp as block_timestamp,
+"fee"::float as fee,
+"input_value"::float as input_value,
+"output_value"::float as output_btc,
+round("fee"/"size")::float as fee_per_byte,
+"is_coinbase"::boolean as is_coinbase,
+"outputs"::variant as outputs
+from {{ source('bitcoin_stg','tblbitcoin_stg_raw') }}  
+
+ 
+
+)
+select * from bitcoin_stg
